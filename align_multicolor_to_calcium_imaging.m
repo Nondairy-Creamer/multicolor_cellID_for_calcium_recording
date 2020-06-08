83f2e7d2cca72f08a51946b9d4702c8bcee8b9db
Matthew Creamer <matthew.s.creamer@gmail.com>
2020-06-08 14:04:32 -0400

    Initial commit to get files into git
    Nothing works yet, the functions will be combined into the align_multicolor_to_calcium_imaging file

---
+++ align_multicolor_to_calcium_imaging.m
@@ -0,0 +1,20 @@
+function align_multicolor_to_calcium_imaging()
+    %% select multicolor folder and calcium data folder
+    
+    
+    %% make AML format from calcium data
+    
+    
+    %% use NeuroPAL software to extract cell body locations from calcium data
+    
+    
+    %% align multicolor and calcium imaging
+    
+    
+    %% align calcium multicolor cell bodies and our cell bodies
+    
+    
+    %% create a spreadsheet with cell body locations and IDs
+
+
+end
