49e19de8104e00ccdbb577f375f6ba096b60069f
Matthew Creamer <matthew.s.creamer@gmail.com>
2021-04-29 11:41:46 -0400

    Got rid of try-catch in create_multicolor_adjustment_file

--- create_multicolor_adjustment_file.m
+++ create_multicolor_adjustment_file.m
@@ -25,50 +25,44 @@ function create_multicolor_adjustment_file()
     end
     
     for ff = 1:num_files
-        try
-            data_folder = multicolor_folders(ff).folder;
-            
-            % get multicolor data from .dat file
-            data = load_multicolor_from_dat(data_folder);
-            data = permute(data, [2, 1, 3, 4]);
+        data_folder = multicolor_folders(ff).folder;
 
-            % find the channels needed for the multicolor image
-            channels_to_use = config.channels_to_use;
-            channel_order = readmatrix(fullfile(data_folder, 'moleculesSequence.txt'), 'OutputType', 'char', 'Delimiter', ',');    
-            channel_ind = zeros(length(channels_to_use), 1);
+        % get multicolor data from .dat file
+        data = load_multicolor_from_dat(data_folder);
+        data = permute(data, [2, 1, 3, 4]);
 
-            for cc = 1:length(channels_to_use)
-                for co = 1:length(channel_order)
-                    if strcmp(channels_to_use{cc}, channel_order{co})
-                        channel_ind(cc) = co;
-                    end
+        % find the channels needed for the multicolor image
+        channels_to_use = config.channels_to_use;
+        channel_order = readmatrix(fullfile(data_folder, 'moleculesSequence.txt'), 'OutputType', 'char', 'Delimiter', ',');    
+        channel_ind = zeros(length(channels_to_use), 1);
+
+        for cc = 1:length(channels_to_use)
+            for co = 1:length(channel_order)
+                if strcmp(channels_to_use{cc}, channel_order{co})
+                    channel_ind(cc) = co;
                 end
             end
-
-            if any(channel_ind == 0)
-                error('Could not find one of the channels required for multicolor imaging');
-            end
-
-            % convert channels from 0 to 1 indexing
-            data = data(:, :, :, channel_ind);
-
-            % spatially align the data to the panneuronal marker
-            data = align_color_channels(data);
-            
-            % get z step size
-            piezo_position = csvread(fullfile(data_folder, 'piezoPosition.txt'));
-            % this file is in fraction of 200 micrometer, convert to micrometers
-            p_step = 200*(piezo_position(2) - piezo_position(1));
-
-            default_struct = get_default_adjustment_file(data);
-            default_struct.scale(3) = p_step;
-
-            save_path = fullfile(data_folder, 'multicolor_adjustment.mat');
-            save(save_path, '-struct', 'default_struct');
-        
-        catch err
-            disp(err);
-            warning(['error in folder ' multicolor_folders(ff).folder ' ignoring and trying next folder']);
         end
+
+        if any(channel_ind == 0)
+            error('Could not find one of the channels required for multicolor imaging');
+        end
+
+        % convert channels from 0 to 1 indexing
+        data = data(:, :, :, channel_ind);
+
+        % spatially align the data to the panneuronal marker
+        data = align_color_channels(data);
+
+        % get z step size
+        piezo_position = csvread(fullfile(data_folder, 'piezoPosition.txt'));
+        % this file is in fraction of 200 micrometer, convert to micrometers
+        p_step = 200*(piezo_position(2) - piezo_position(1));
+
+        default_struct = get_default_adjustment_file(data);
+        default_struct.scale(3) = p_step;
+
+        save_path = fullfile(data_folder, 'multicolor_adjustment.mat');
+        save(save_path, '-struct', 'default_struct');
     end
 end
