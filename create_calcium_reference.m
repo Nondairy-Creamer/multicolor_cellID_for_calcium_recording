f1dc76bfaf2fe6b6ae407bf7c8586899d876eff6
Matthew Creamer <matthew.s.creamer@gmail.com>
2021-07-01 15:47:39 -0400

    Finalized GUI for mobile worm calcium references

--- create_calcium_reference.m
+++ create_calcium_reference.m
@@ -1,4 +1,4 @@
-function create_calcium_reference_immobile()
+function create_calcium_reference()
     % create_calcium_reference  Average together the first n volumes of a
     % calcium recording together. We will use this average volume to get
     % initial cell body locations
