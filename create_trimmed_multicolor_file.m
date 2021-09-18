bd5462dfd3759e13f44236a5d87cdd069a69fb18
Matthew S. Creamer <mcreamer@tigressdata2.princeton.edu>
2021-09-18 15:47:10 -0400

    gui should be fully functional but deletes unassigned neurons on every save

--- create_trimmed_multicolor_file.m
+++ create_trimmed_multicolor_file.m
@@ -10,8 +10,9 @@ function create_trimmed_multicolor_file(data_folder)
     end
     
     %% make neuropal_data_trimmed with only tracked cells marked
-    alignment_path = fullfile(data_folder, 'calcium_to_multicolor_alignment.mat');
-    alignment = load(alignment_path);
+    assignment_path = fullfile(data_folder, 'calcium_to_multicolor_assignments.mat');
+    loaded_assignments = load(assignment_path);
+    assignments = loaded_assignments.assignments;
     
     multicolor_search = dir(fullfile(data_folder, 'multicolor*'));
     multicolor_search = multicolor_search(1); % take only the first result. Can't handle multiple multicolor folders in one file
@@ -19,14 +20,13 @@ function create_trimmed_multicolor_file(data_folder)
     multicolor_cell_locations = load(multicolor_cell_locations_path);
     
     num_multicolor_cells = length(multicolor_cell_locations.neurons.neurons);
-    tracked_cell_assignments = alignment.assignments.tracked_to_multicolor_assignments_user_adjusted;
-    num_tracked_cells = length(tracked_cell_assignments);
+    num_tracked_cells = length(assignments);
     multicolor_cell_is_tracked = false(num_multicolor_cells, 1);
     
     for nn = 1:num_tracked_cells
         % find out if this multicolor cell was assigned to a tracked cell
-        if tracked_cell_assignments(nn) ~= 0
-            multicolor_cell_is_tracked(tracked_cell_assignments(nn)) = true;
+        if assignments(nn) ~= 0
+            multicolor_cell_is_tracked(assignments(nn)) = true;
         end
     end
     
@@ -34,12 +34,7 @@ function create_trimmed_multicolor_file(data_folder)
     multicolor_cell_locations.neurons.neurons = multicolor_cell_locations.neurons.neurons(multicolor_cell_is_tracked);   
     
     %% save the files
-    % copy over the existing neuropal data images
-    orig_path = fullfile(multicolor_search.folder, multicolor_search.name, 'neuropal_data.mat');
-    copy_path = fullfile(multicolor_search.folder, multicolor_search.name, 'neuropal_data_trimmed.mat');
-    copyfile(orig_path, copy_path);
-    
-    cell_id_save_path = fullfile(multicolor_search.folder, multicolor_search.name, 'neuropal_data_trimmed_ID.mat');
+    cell_id_save_path = fullfile(multicolor_search.folder, multicolor_search.name, 'neuropal_data_ID.mat');
     
     struct_h = whos('multicolor_cell_locations');
     bytes_in_struct = struct_h.bytes;
